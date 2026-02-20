73446e3416d99cea96b2e232faa52a78e738e1c3
Evan H <evanh@evan-pc.lan>
2026-02-19 19:11:08 -0800

    removed relative path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/klept/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: './', // Using relative paths for better compatibility with GH Pages
+  base: 'klept', // Using relative paths for better compatibility with GH Pages
 })
